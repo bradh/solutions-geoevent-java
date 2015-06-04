965206f096c46f25e643ce07f8f77acf62c8d579
Patrick Hill <patrick_hill@esri.com>
2015-06-04 10:56:36 -0700

    initial commit mlobi transport and adapter

---
+++ solutions-geoevent/transports/mlobi-transport/src/main/java/com/esri/geoevent/solutions/transport/mlobi/App.java
@@ -0,0 +1,13 @@
+package com.esri.geoevent.solutions.transport.mlobi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
