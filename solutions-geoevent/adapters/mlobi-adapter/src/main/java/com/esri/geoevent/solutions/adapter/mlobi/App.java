6adb811d56355f0da2ca8fdebef3c0c6d6770b70
Patrick Hill <patrick_hill@esri.com>
2015-07-09 15:48:53 -0700

    initial commit molobi inbound adapter

---
+++ solutions-geoevent/adapters/mlobi-adapter/src/main/java/com/esri/geoevent/solutions/adapter/mlobi/App.java
@@ -0,0 +1,13 @@
+package com.esri.geoevent.solutions.adapter.mlobi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
